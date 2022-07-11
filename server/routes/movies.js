837163bac8aa59aed20c082a865805b3d31d5288
mschwartz8 <42093190+mschwartz8@users.noreply.github.com>
2022-07-11 17:18:48 -0400

    transfered sendFile to html file

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -20,10 +20,46 @@ router.get("/", (req, res) => {
 
 // GET /movies/add-movie
 // respond with HTML text to be rendered by the browser that will show a form
-router.get("/add-movie", (req, res) => {
+router.get("/add-movie", async (req, res) => {
     // express method sendFile that allows you to send back a specific file 
     // __dirname is current directory name editing in (routes dir) + specific part
-    res.sendFile(__dirname + "/views/movie-form.html")
+    // res.sendFile(__dirname + "/views/movie-form.html")
+    res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Add a movie to your watchlist</title>
+        <link rel="stylesheet" type="text/css" href="/base-styling.css" />
+    </head>
+    <body>
+        <h1>Add movie</h1>
+        <form method="POST" action="/movies">
+            <div>
+                <label>Title:</label>
+                <input type="text" name="title" />
+            </div>
+            <div>
+                <label>IMDB link:</label>
+                <input type="text" name="link" placeholder="Optional" />
+            </div>
+            <div>
+                <div id="genre-selects-container">
+                    <select id="genre-select" name="genres">
+                        <option></option>
+                        ${allOfMyGenres.map(genre => {
+    return `<option value="${genre.id}">${genre.name}</option>`
+}).join("")
+    }
+                    </select>
+                </div>
+                <button type="button" id="add-button">+</button>
+            </div>
+            <button type="submit">Add Movie</button>
+        </form>
+        <script type="text/javascript" src="/movie-form.js"></script>
+    </body>
+    </html>
+`);
 })
 
 // POST /movies
