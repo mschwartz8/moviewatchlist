9100cc8ac648715179c8d9815df64b649f5a7f6e
mschwartz8 <42093190+mschwartz8@users.noreply.github.com>
2022-07-16 13:01:32 -0400

    update seed file with movies

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,7 +1,37 @@
-const dbConnection = require("./index").dbConnection;
+// const dbConnection = require("./index").dbConnection; same as const {dbConnection} = require("./index");
+const {dbConnection, Movie, Genre } = require("./index")
 
 const runSeed = async () => {
     await dbConnection.sync({ force: true });
+
+    const horrorGenre = await Genre.create({name: "Horror"})
+    const comedyGenre = await Genre.create({name: "Comedy"})
+    const actionGenre = await Genre.create({name: "Action"})
+    const dramaGenre = await Genre.create({name: "Drama"})
+
+    const EverythingEverywhereAllAtOnce = await Movie.create({
+        title: "Everything Everywhere All at Once",
+        imbdLink: "https://www.imdb.com/title/tt6710474/?ref_=nv_sr_srsg_0"
+    });
+
+    const Parasite = await Movie.create({
+        title: "Parasite",
+    });
+
+    const MidSommar = await Movie.create({
+        title: "MidSommar",
+    });
+
+    const WhereTheCrawdadsSing = await Movie.create({
+        title: "Where the Crawdads Sing",
+    });
+
+    // Sequelize magic method, could put ids or variables in setGenres
+    await EverythingEverywhereAllAtOnce.setGenres([comedyGenre, actionGenre, dramaGenre])
+    await Parasite.setGenres([actionGenre, dramaGenre])
+    await MidSommar.setGenres([horrorGenre])
+    await WhereTheCrawdadsSing.setGenres([dramaGenre])
+
     console.log('seed is complete!');
     process.kill(0);
 }
