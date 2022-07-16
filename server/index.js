2aa06d2e216505809decee97f0596bfec73d4174
mschwartz8 <42093190+mschwartz8@users.noreply.github.com>
2022-07-16 11:52:15 -0400

    added home page html

--- server/index.js
+++ server/index.js
@@ -41,5 +41,19 @@ app.use("/movies", moviesRouter)
 
 
 app.get("/", (req, res) => {
-    res.send("Hello :)")
+    res.send(
+        
+        `
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Movie WatchList App</title>
+            </head>
+            <body>
+                <div id="hey-react-put-your-app-here"></div>
+                <script src="/bundle.js"></script>
+            </body>
+        </html>
+        `
+    )
 })
